19f68b6801a809f9e8925e12fa14723e9c731678
Ashley Hodge <ashhodge@comcast.net>
2022-03-17 19:49:45 -0400

    add put and delete routes to category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,17 +1,26 @@
 const router = require('express').Router();
 const { Category, Product } = require('../../models');
+const { restore } = require('../../models/Product');
 
 // The `/api/categories` endpoint
 
 router.get('/', (req, res) => {
   // find all categories
-  Category.findAll()
+  Category.findAll({
+    // be sure to include its associated Products
+    // include: [
+    //   {
+    //     model: Product,
+    //     attributes: ['product_name']
+    //   }
+    // ]
+  })
   .then(dbCategoryData => res.json(dbCategoryData))
   .catch(err => {
     console.log(err);
     res.json(500);
   });
-  // be sure to include its associated Products
+  
 });
 
 router.get('/:id', (req, res) => {
@@ -19,7 +28,14 @@ router.get('/:id', (req, res) => {
   Category.findOne({
     where: {
       id: req.params.id
-    }
+    },
+    // be sure to include its associated Products
+    // include: [
+    //   {
+    //     model: Product,
+    //     attributes: ['product_name']
+    //   }
+    // ]
   })
   .then(dbCategoryData => {
     if(!dbCategoryData) {
@@ -32,19 +48,63 @@ router.get('/:id', (req, res) => {
     console.log(err);
     res.status(500).json(err);
   });
-  // be sure to include its associated Products
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create({
+    category_name: req.body.category_name
+  })
+  .then(dbCategoryData => res.json(dbCategoryData))
+  .catch(err => {
+    console.log(err);
+    restore.status(500)
+  })
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(
+    {
+      category_name: req.body.category_name
+    },
+    {
+      where: {
+        id: req.params.id
+      }
+    }
+  )
+  .then(dbCategoryData => {
+    if(!dbCategoryData) {
+      res.status(404).json({ message: 'No category found at this id!' });
+      return;
+    }
+    res.json(dbCategoryData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(dbCategoryData => {
+    if(!dbCategoryData) {
+      res.status(400).json({ message: 'No category found at this id!' });
+      return;
+    }
+    res.json(dbCategoryData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 module.exports = router;
